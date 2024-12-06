8734688621f74aed875a4817bdd988a2fd3e4b38
Pratik4555-star <pratikmahadik554@gmail.com>
2024-12-06 17:25:25 +0530

    cursor animation change for mobile view

--- src/cursor/CustomCursor.jsx
+++ src/cursor/CustomCursor.jsx
@@ -1,20 +1,13 @@
-import  { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
 const CustomCursor = () => {
-  // const cursorRef = useRef(null);
   const followerRef = useRef(null);
 
   useEffect(() => {
-    // const cursor = cursorRef.current;
     const follower = followerRef.current;
 
     const moveCursor = (e) => {
-      // gsap.to(cursor, {
-      //   x: e.clientX,
-      //   y: e.clientY,
-      //   duration: 0,
-      // });
       gsap.to(follower, {
         x: e.clientX,
         y: e.clientY,
@@ -22,7 +15,10 @@ const CustomCursor = () => {
       });
     };
 
-    window.addEventListener("mousemove", moveCursor);
+    // Check if the screen width is greater than 768px (desktop view)
+    if (window.innerWidth > 768) {
+      window.addEventListener("mousemove", moveCursor);
+    }
 
     return () => {
       window.removeEventListener("mousemove", moveCursor);
@@ -31,20 +27,6 @@ const CustomCursor = () => {
 
   return (
     <>
-      {/* <div
-        ref={cursorRef}
-        style={{
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "8px",
-          height: "8px",
-          backgroundColor: "white",
-          borderRadius: "50%",
-          pointerEvents: "none",
-          zIndex: 9999,
-        }}
-      /> */}
       <div
         ref={followerRef}
         style={{
@@ -65,16 +47,3 @@ const CustomCursor = () => {
 };
 
 export default CustomCursor;
-
-
-/* height: 20px;
-    width: 20px;
-    background-color: aliceblue;
-    border-radius: 50%;
-    position:fixed;
-    font-size: 6px;
-    display: flex;
-    justify-content: center;
-    text-align: center;
-    align-items: center;
-    z-index: 9; */
