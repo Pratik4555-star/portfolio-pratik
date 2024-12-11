ef624ecf49b58d065a22d7a5f24fdfffa600c343
Pratik4555-star <pratikmahadik554@gmail.com>
2024-12-11 16:30:22 +0530

    `Refactor Startup component: update background color and styles`

--- src/components/Startup.jsx
+++ src/components/Startup.jsx
@@ -6,7 +6,7 @@ function Startup() {
         <div className="flex justify-center items-center">
           <h1 className=" text-[#999596] font-inner md:text-9xl py-60"> Coming Soon....</h1>
         </div>
-        <div className=" relative py-20 w-full flex items-center justify-center">
+        <div className=" relative bg-[#191919] py-20 w-full flex items-center justify-center">
           <h1 className="font-inner text-lg sm:text-2xl md:text-4xl text-[#999596] mb-10 font-bold text-center px-4">
             Contact: pratikmahadik554@gmail.com
           </h1>
